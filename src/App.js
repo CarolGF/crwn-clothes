970434bde2b636d469060745090a4ac3e0c6a832
CarolGF <karola_gf@hotmail.com>
2020-05-03 15:36:17 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Homepage from "./pages/Homepage/Homepage";
+import "./App.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Homepage />
+      </div>
+    );
+  }
+}
+
+export default App;
