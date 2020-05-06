19e6bc5784612128899739efc3e3a017b7ab5ab7
CarolGF <karola_gf@hotmail.com>
2020-05-06 19:36:02 +0100

    implementing firebase

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
 import SignIn from "../../components/Sign-In/Sign-In";
+import SignUp from "../../components/Sign-Up/Sign-Up";
 import "./SignInPage.scss";
 
 const SignInPage = () => {
-  return <SignIn />;
+  return (
+    <div className='sign-in-page'>
+      <SignIn />
+      <SignUp />
+    </div>
+  );
 };
 
 export default SignInPage;
