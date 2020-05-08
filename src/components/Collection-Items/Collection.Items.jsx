19d7c04ec8391c09a3540b205b37d342bd237fb9
CarolGF <karola_gf@hotmail.com>
2020-05-08 19:17:36 +0100

    updating cart utils

--- src/components/Collection-Items/Collection.Items.jsx
+++ src/components/Collection-Items/Collection.Items.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { connect } from "react-redux";
+import { addItem } from "../../redux/cart/cart-actions";
 
+import CustomButton from "../Custom-Button/Custom-Button";
 import "./Collection-Items.scss";
 
-const CollectionItems = ({ id, name, price, imageUrl }) => {
+const CollectionItems = ({ item, addItem }) => {
+  const { imageUrl, name, price } = item;
   return (
     <div className='collection-item'>
       <div className='image' style={{ backgroundImage: `url(${imageUrl})` }} />
@@ -10,8 +14,15 @@ const CollectionItems = ({ id, name, price, imageUrl }) => {
         <span className='name'>{name}</span>
         <span className='price'>{price}</span>
       </div>
+      <CustomButton onClick={() => addItem(item)} inverted>
+        Add to cart
+      </CustomButton>
     </div>
   );
 };
 
-export default CollectionItems;
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItems);
